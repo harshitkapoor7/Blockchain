bfc2271b107e894266395e29c04b78213fcbc301
Harshit <meharshitkapoor7@gmail.com>
2021-03-27 13:59:04 +0530

    Initial Commit

---
+++ app/pubsub.js
@@ -0,0 +1,74 @@
+const PubNub = require('pubnub');
+
+const credentials = {
+    publishKey: 'pub-c-edd29309-3bb0-452d-83e0-41e0b30ec5af',
+    subscribeKey: 'sub-c-6140050c-874c-11eb-a540-2a2aff149455',
+    secretKey: 'sec-c-ZWUxNTQ2ODItNjY4Mi00ZWU1LWFlYWMtOTRhMmUyYWFiZjBi'
+};
+
+
+const CHANNELS = {
+    TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION: 'TRANSACTION'
+};
+
+class PubSub {
+    constructor({ blockchain, transactionPool, wallet }) {
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+
+        this.pubnub = new PubNub(credentials);
+
+        this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+        this.pubnub.addListener(this.listener());
+    }
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
+    }
+
+    broadcastTransaction(transaction){
+        this.publish({
+            channel: CHANNELS.TRANSACTION,
+            message: JSON.stringify(transaction)
+          });
+    }
+
+    listener() {
+        return {
+            message: messageObject => {
+                const { channel, message } = messageObject;
+                console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+                
+                const parsedMessage = JSON.parse(message);
+                switch(channel){
+                    case CHANNELS.BLOCKCHAIN:
+                        this.blockchain.replaceChain(parsedMessage);
+                        break;
+                    case CHANNELS.TRANSACTION:
+                        if (!this.transactionPool.existingTransaction({
+                            inputAddress: this.wallet.publicKey
+                          })) {
+                            this.transactionPool.setTransaction(parsedMessage);
+                          }
+              
+                        break;
+                    default:
+                        return;
+                }
+            }
+        };
+    }
+
+    publish({ channel, message }) {
+        this.pubnub.publish({ message, channel });
+    }
+}
+
+module.exports = PubSub;
---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,35 @@
+const Wallet = require('./index');
+const Transaction = require('./transaction');
+const TransactionPool = require('./transaction-pool');
+
+describe('TransactionPool', () => {
+    let transactionPool, transaction,senderWallet;
+
+    beforeEach(() => {
+        senderWallet = new Wallet();
+        transactionPool = new TransactionPool();
+        transaction = new Transaction({
+            senderWallet,
+            recipient: 'fake-recipient',
+            amount: 50
+        });
+    });
+
+    describe('setTransaction()',()=>{
+        it('adds a transaction',()=>{
+            transactionPool.setTransaction(transaction);
+
+            expect(transactionPool.transactionMap[transaction.id])
+            .toBe(transaction);
+        });
+    });
+
+    describe('existingTransaction()',()=>{
+        it('returns an existing transaction given an input address',()=>{
+            transactionPool.setTransaction(transaction);
+            expect(
+                transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})
+            ).toBe(transaction);
+        });
+    });
+});
